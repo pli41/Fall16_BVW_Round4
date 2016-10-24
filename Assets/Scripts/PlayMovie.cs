c3b28156cb1035aa72b54f27973c8d4aaf545857
Griva Patel <gpatel@andrew.ad.cmu.edu>
2016-10-23 22:18:23 -0400

    latest changes

---
+++ Assets/Scripts/PlayMovie.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayMovie : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        MovieTexture m = (MovieTexture)GetComponent<RawImage>().texture;
+        m.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
